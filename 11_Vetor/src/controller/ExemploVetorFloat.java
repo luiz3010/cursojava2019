dd82e41d3a806f62dfda7952e7c1625345405b13
Aluno <Aluno@ALU24254.educa.sf>
2020-01-10 21:40:03 -0300

    Vetor_11

---
+++ 11_Vetor/src/controller/ExemploVetorFloat.java
@@ -0,0 +1,16 @@
+package controller;
+
+public class ExemploVetorFloat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float notas[]= {4,7.5f,10,9};
+System.out.println("notas[0]="+notas[0]);
+System.out.println("notas[1]="+notas[1]);
+System.out.println("notas[2]="+notas[2]);
+System.out.println("notas[3]="+notas[3]);
+
+
+	}
+
+}
